08590b63ac578558c40a4170411b5b74945f114e
Shubham <shubhwankar88@gmail.com>
2025-07-30 15:54:19 +0530

    Link expression

--- src/main/java/com/demo/Controller/UserController.java
+++ src/main/java/com/demo/Controller/UserController.java
@@ -8,26 +8,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
+    // variable expressions
     @GetMapping("/variable-expression")
-    public String variableExpression(Model model){
+    public String variableExpression(Model model) {
         User user = new User("Shubham", "shubh@gmail.com", "ADMIN", "male");
         model.addAttribute("user", user);
         return "variable-expression";
     }
 
+    // selection expression
     @GetMapping("/selection-expression")
-    public String selectionExpression(Model model){
+    public String selectionExpression(Model model) {
         User user = new User("Shubham", "shubh@gmail.com", "ADMIN", "male");
         model.addAttribute("user", user);
         return "selection-expression";
     }
 
+    // message expression
     // message expressions are used to access text from message.properties file
     @GetMapping("/message-expression")
-    public String messageExpression(){
+    public String messageExpression() {
         return "message-expression";
     }
 
-
+    // link expression
+    @GetMapping("/link-expression")
+    public String linkExpression(Model model) {
+        model.addAttribute("id", 1);
+        return "link-expression";
+    }
 
 }
